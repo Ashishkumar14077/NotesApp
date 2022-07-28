8918770e08382a387a8cf180ad90a91026b39d2d
Ashish <pintubrajkishor@gmail.com>
2022-07-28 14:19:29 +0530

    functonality done

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors")
 
-app.use(express.json());
-app.use(cors())
 
+app.use(cors())
+app.use(express.json());
+app.use(express.static('./client/public'))
 //Routes
 const user = require("./routes/userRouter");
 const note = require("./routes/notesRouter");
